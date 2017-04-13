327fd3802abca59da16884985bb0e342f9747f78
willsam100 <willsam100@gmail.com>
2017-04-14 01:57:40 +1200

    Folder renames

--- IntroToXamarinToF#ers/Script.fsx
+++
@@ -1,7 +0,0 @@
-#r "System.ServiceModel"
-#r "/Users/Ashley/code/FsReveal-master/packages/FSharp.Data.TypeProviders/lib/net40/FSharp.Data.TypeProviders.dll"
-open FSharp.Data.TypeProviders
-
-type Wsdl1 = WsdlService<"http://api.microsofttranslator.com/V2/Soap.svc">
-
-let ctxt = Wsdl1.GetBasicHttpBinding_LanguageService()
